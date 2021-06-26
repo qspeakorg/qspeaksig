9f1a9fcd465339f2629f9b44ae58fd9e38e2c0bf
qspeakorg <85754186+qspeakorg@users.noreply.github.com>
2021-06-26 01:07:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function myFunction() {
   var email = document.getElementById("frm1").email.value;
   var footer = document.getElementById("frm1").footer.value;
     
-  document.getElementById("name").innerHTML = name;
   if (pronouns.length > 0) {
-    document.getElementById("pronouns").innerHTML = "(".concat(pronouns, ")");
+    var pro_text = " (".concat(pronouns, ")");
   } else {
-      document.getElementById("pronouns").innerHTML = "";
+    var pro_text = "";
   };
+  document.getElementById("name").innerHTML = name.concat(pro_text);
   document.getElementById("position").innerHTML = position;
   if (email.length > 0) {
       document.getElementById("email").innerHTML = email;
@@ -22,4 +22,49 @@ function myFunction() {
         email_link.setAttribute('href', 'mailto:lgbt.colourfully@gmail.com');
   };
   document.getElementById("footer").innerHTML = footer;
-}
+};
+
+function reset () {
+  var name = "Team QSpeak";
+  var pronouns = "";
+  var position = "";
+  var email = "";
+  var footer = "Acknowledging that I live and work in the Dish with One Spoon Territory, the home of the Anishnaabe, Haudenosaunee and Missisaugas of the Credit.";
+    
+  if (pronouns.length > 0) {
+    var pro_text = " (".concat(pronouns, ")");
+  } else {
+    var pro_text = "";
+  };
+  document.getElementById("name").innerHTML = name.concat(pro_text);
+  document.getElementById("position").innerHTML = position;
+  if (email.length > 0) {
+      document.getElementById("email").innerHTML = email;
+      var email_link = document.getElementById('email');
+        email_link.setAttribute('href', 'mailto:'.concat(email));
+  } else {
+      document.getElementById("email").innerHTML = "lgbt.colourfully@gmail.com";
+      var email_link = document.getElementById('email');
+        email_link.setAttribute('href', 'mailto:lgbt.colourfully@gmail.com');
+  };
+  document.getElementById("footer").innerHTML = footer;
+};
+
+function copy_sig() {
+    var target = document.getElementById('signature_id');
+      var range, select;
+      if (document.createRange) {
+        range = document.createRange();
+        range.selectNode(target)
+        select = window.getSelection();
+        select.removeAllRanges();
+        select.addRange(range);
+        document.execCommand('copy');
+        select.removeAllRanges();
+      } else {
+        range = document.body.createTextRange();
+        range.moveToElementText(target);
+        range.select();
+        document.execCommand('copy');
+      }
+};
